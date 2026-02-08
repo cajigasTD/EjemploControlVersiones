cd0a6354c09e2d600a67c400444d32a151945c48
cajigasTD <cajigas@alumni.uv.es>
2026-02-08 22:33:40 +0100

    return ((L+l)*2)

--- Perim.R
+++ Perim.R
@@ -4,6 +4,6 @@ AreaCuadrado <- function(L){
 LongitudCircunf <- function(R) {
   2 * 3.14159 * R
 }
-PerimetroRectangulo <- function(base, altura) {
-  2 * (base + altura)
+PerimetroRectangulo <- function(L, l) {
+  (L + l) * 2
 }
